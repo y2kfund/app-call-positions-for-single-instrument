6097772d77046bf6ea5a7545bb47049e66071d07
raj <raj@administrators-iMac.local>
2025-11-11 21:20:36 +0530

    Update position package

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import { default as callPositions } from './views/Positions.vue';
+export { callPositions };
+export default callPositions;
+export interface callPositionsProps {
+    symbolRoot: string;
+    userId?: string | null;
+}
---
+++ dist/views/Positions.vue.d.ts
@@ -0,0 +1,11 @@
+interface callPositionsProps {
+    symbolRoot: string;
+    userId?: string | null;
+}
+declare const _default: import('vue').DefineComponent<callPositionsProps, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<callPositionsProps> & Readonly<{}>, {
+    symbolRoot: string;
+    userId: string | null;
+}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {
+    tableDiv: HTMLDivElement;
+}, HTMLDivElement>;
+export default _default;
