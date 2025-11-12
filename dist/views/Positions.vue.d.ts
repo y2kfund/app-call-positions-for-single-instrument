e1959ed661faf6f4d160d07c223ca2cbeaf249e8
raj <raj@administrators-iMac.local>
2025-11-12 21:47:58 +0530

    Improve symbol details page

--- dist/views/Positions.vue.d.ts
+++ dist/views/Positions.vue.d.ts
@@ -7,5 +7,6 @@ declare const _default: import('vue').DefineComponent<callPositionsProps, {}, {}
     userId: string | null;
 }, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {
     tableDiv: HTMLDivElement;
+    expiredTableDiv: HTMLDivElement;
 }, HTMLDivElement>;
 export default _default;
